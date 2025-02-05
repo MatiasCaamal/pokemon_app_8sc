2e6e6ef22ef7238a81e3a3f4bf51625bc510273c
MatiasCaamal <matiascaamal@hotmail.com>
2025-02-04 23:46:40 -0600

    Iniciando UI

--- pokemon_app_8sc/lib/button.dart
+++ pokemon_app_8sc/lib/button.dart
@@ -31,6 +31,5 @@ class MyButton extends StatelessWidget {
         ),
       ),
     );
-    
   }
 }
--- pokemon_app_8sc/lib/main.dart
+++ pokemon_app_8sc/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'button.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,17 +21,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  void moveUp() {
+    
+  }
+
+  void moveDown() {
+    
+  }
+
+  void moveLeft() {
+    
+  }
+
+  void moveRight() {
+    
+  }
+
+  void pressedA() {
+    
+  }
+
+  void pressedB() {
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-         AspectRatio(
-           aspectRatio: 1,
-           child: Container(
-             color: Colors.black,
-             ),
-           ),
+          AspectRatio(
+            aspectRatio: 1,
+            child: Container(
+              color: Colors.black,
+            ),
+          ),
           Expanded(
             child: Container(
               color: Colors.grey[800],
@@ -39,16 +65,101 @@ class _HomePageState extends State<HomePage> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text('G A M E B O Y', style: TextStyle(color: Colors.white),),
-                    Expanded(
-                      child: Container(
-                        color: Colors.red,
-                        ),
+                    Text(
+                      'G A M E B O Y',
+                      style: TextStyle(color: Colors.white),
                     ),
-                      Text('C R E A T E D B Y M A T I A S', 
-                      style: TextStyle(color: Colors.white),)
-                    ],
-                  ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Row(
+                          children: [
+                            Column(
+                              children: [
+                                SizedBox(
+                                  height: 50,
+                                  width: 50,
+                                ),
+                                MyButton(
+                                  text: '⬅',
+                                  function: moveLeft,
+                                ),
+                                SizedBox(
+                                  height: 50,
+                                  width: 50,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                MyButton(
+                                  text: '⬆',
+                                  function: moveUp,
+                                ),
+                                SizedBox(
+                                  height: 50,
+                                  width: 50,
+                                ),
+                                MyButton(
+                                  text: '⬇',
+                                  function: moveDown,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                SizedBox(
+                                  height: 50,
+                                  width: 50,
+                                ),
+                                MyButton(
+                                  text: '➡',
+                                  function: moveRight,
+                                ),
+                                SizedBox(
+                                  height: 50,
+                                  width: 50,
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Column(
+                              children: [
+                                SizedBox(
+                                  height: 50,
+                                  width: 50,
+                                ),
+                                MyButton(
+                                  text: 'b',
+                                  function: pressedB,
+                                )
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                MyButton(
+                                  text: 'a',
+                                  function: pressedA,
+                                ),
+                                SizedBox(
+                                  height: 50,
+                                  width: 50,
+                                )
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    Text(
+                      'C R E A T E D B Y M A T I A S',
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
